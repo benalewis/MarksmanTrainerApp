4e03645b98663c1b180076153d3c4a9609f0da03
Ben Lewis <benalewis@hotmail.co.uk>
2016-05-16 19:37:23 +0100

    Added Barebones
    Started with basic numbers

--- MainActivity.java
+++ MainActivity.java
@@ -5,8 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Integer> answers = new ArrayList<Integer>();
+    int locationOfCorrect;
+    TextView champTextView;
+    TextView resultTextView;
+    TextView scoreTextView;
+    int score;
+
+    Button a;
+    Button b;
+    Button c;
+    Button d;
+
+    public void chooseAnswer(View view) {
+
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrect))) {
+            score++;
+            scoreTextView.setText(Integer.toString(score));
+            resultTextView.setText("Correct!");
+        } else {
+            resultTextView.setText("Wrong!");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +43,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        champTextView = (TextView) findViewById(R.id.champTextView);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        scoreTextView = (TextView) findViewById(R.id.scoreTextView);
 
-        System.out.print("Hello Git!");
+        a = (Button) findViewById(R.id.button);
+        b = (Button) findViewById(R.id.button1);
+        c = (Button) findViewById(R.id.button2);
+        d = (Button) findViewById(R.id.button3);
 
+        Random random = new Random();
+
+        int correctAnswer = random.nextInt(18);
+        int incorrectAnswer;
+
+        locationOfCorrect = random.nextInt(4);
+
+        for (int i = 0; i < 4; i++) {
+
+            if (i == locationOfCorrect) {
+
+                answers.add(correctAnswer);
+
+            } else {
+
+                incorrectAnswer = random.nextInt(18);
+
+                while (incorrectAnswer == correctAnswer) {
+
+                    incorrectAnswer = random.nextInt(18);
+                }
+
+                answers.add(incorrectAnswer);
+            }
+        }
+
+        champTextView.setText(String.valueOf(correctAnswer));
+        a.setText(Integer.toString(answers.get(0)));
+        b.setText(Integer.toString(answers.get(1)));
+        c.setText(Integer.toString(answers.get(2)));
+        d.setText(Integer.toString(answers.get(3)));
     }
 
     @Override
